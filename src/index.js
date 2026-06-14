6927ebe3c4073642848ce5ccc40671de71c3fef0
Swabian-Janne <janne.jasmin.jp@gmail.com>
2026-06-14 20:01:15 +0200

    Corrections in JS

--- src/index.js
+++ src/index.js
@@ -1,30 +1,47 @@
 /** @format */
 
 function displayRecipe(response) {
+  console.log("recipe generated");
   new Typewriter("#recipe", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 50,
+    delay: 1,
     cursor: "",
-    loop: false,
   });
 }
 
 function generateRecipe(event) {
   event.preventDefault();
+
+  
+//build the API URL
+  let ingredientsInput = document.querySelector("#user-ingredients");
+  let apiKey = "ef3aab353f2ado9faa8tccd552b00647";
+  let prompt = `User instructions: Generate a recipe based on the ingredients the user has on hand. ${ingredientsInput.value}.`;
+  let context =
+    "You are a chef who creates simple recipes based on the ingredients the user has on hand. Your mission is to generate a recipe that is easy to follow, simple and delicious. Make sure to follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
   let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
   recipeElement.innerHTML = "Your recipe is being put togehter...";
+  
+    console.log("generating recipe...");
+    console.log("Prompt: ${prompt}");
+    console.log("Context: ${context}");
+// call to the API with axios
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
 
-let ingredientsInput = document.querySelector("#user-ingredients");
-let apiKey = "ef3aab353f2ado9faa8tccd552b00647";
-let prompt =
-  "User instructions: Generate a recipe based on the ingredients the user has on hand. ${ingredientsInput.value}.";
-let context =
-  "You are a chef who creates simple recipes based on the ingredients the user has on hand. Your mission is to generate a recipe that is easy to follow, simple and delicious. Make sure to follow the user instructions.";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+let input = document.querySelector("#user-ingredients");
+let hint = document.querySelector(".hint");
 
-axios.get(apiUrl).then(displayRecipe);
+input.addEventListener("input", function () {
+  if (input.value.length > 0) {
+    hint.style.display = "none";
+  } else {
+    hint.style.display = "inline-block";
+  }); 
